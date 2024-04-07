96c648f5b3fc67c750f77e0d46652f4c7d9ec778
najar <hajer.najar.ing@gmail.com>
2024-04-07 13:14:51 +0200

    étape 2 : importer les datas , étape 3 : intégrer la page d'acceuil , ajouter le lien vers la page photographe

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,32 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/images/photographers-photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const link = document.createElement('a');
+        link.setAttribute('href', `photographer.html?${id}`);
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute('src', picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const location = document.createElement('h3');
+        location.textContent = `${city}, ${country}`;
+        const p = document.createElement('p');
+        p.textContent = tagline;
+        const p2 = document.createElement('p');
+        p2.setAttribute('class', 'card-price');
+        p2.textContent = `${price}€/jour`;
+        
+
+        
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(link);
+        article.appendChild(location);
+        article.appendChild(p);
+        article.appendChild(p2);
         return (article);
     }
     return { name, picture, getUserCardDOM }
