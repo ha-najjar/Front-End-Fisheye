07ca50f15d1d66f38feb0a3c93ba2b3b58d6e6d8
najar <hajer.najar.ing@gmail.com>
2024-04-18 12:04:46 +0200

    header de la page photographe, liste des medias du photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,49 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//fonction pour récupérer tout les données depuis le json
+async function getDataFromJson() {
+  const response = await fetch("./data/photographers.json");
+  const dataFromJson = await response.json();
+  // et bien retourner les données seulement une fois récupéré
+  return dataFromJson;
+}
+// fonction pour récupérer un photographe à partir de son id
+async function getPhotographerById(id) {
+  // Récupère les datas des photographes
+  const { photographers } = await getDataFromJson();
+  const photographer = await photographers.find(
+    (_photographer) => _photographer.id === Number(id)
+  );
+  return photographer;
+}
+
+// fonction qui construit la page d'un photographe
+async function buildPhotographerPage(photographer, photographerMedias) {
+  photographerHeader(photographer);
+  // TO DO : for pour parcourir les medias et pour chaque élément appeler un template pour construire un seul media
+  
+  photographerMedias.forEach(media => {
+    mediaTemplate(media, photographer.name);
+  });
+
+}
+  // fonction récupére la liste des médias d'un photographe depuis le json
+  async function getMediasById(id){
+    const { media } = await getDataFromJson();
+    const photographerMedias = await media.filter(
+      (_item) => _item.photographerId === Number(id)
+    );
+    return photographerMedias;
+  }
+
+
+async function init() {
+  // Récupère les id à partir du url
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const id = urlParams.get("id");
+
+  const photographer = await getPhotographerById(id);
+  const photographerMedias = await getMediasById(id);
+  buildPhotographerPage(photographer, photographerMedias);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,33 +1,64 @@
-function photographerTemplate(data) {
-    const { name, portrait, city, country, tagline, price, id } = data;
+function photographerHeader(photographer) {
+  const { name, portrait, city, country, tagline } = photographer;
+  const picture = `assets/images/photographers-photos/${portrait}`;
 
-    const picture = `assets/images/photographers-photos/${portrait}`;
+  const photographerName = document.querySelector(".photographer-name");
+  const photographerLocation = document.querySelector(".photographer-location");
+  const photographerTagline = document.querySelector(".photographer-tagline");
+  const photographerImage = document.querySelector(".photographer-image");
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const link = document.createElement('a');
-        link.setAttribute('href', `photographer.html?${id}`);
-        const img = document.createElement( 'img' );
-        img.setAttribute('src', picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        const location = document.createElement('h3');
-        location.textContent = `${city}, ${country}`;
-        const p = document.createElement('p');
-        p.textContent = tagline;
-        const p2 = document.createElement('p');
-        p2.setAttribute('class', 'card-price');
-        p2.textContent = `${price}€/jour`;
-        
-
-        
-        link.appendChild(img);
-        link.appendChild(h2);
-        article.appendChild(link);
-        article.appendChild(location);
-        article.appendChild(p);
-        article.appendChild(p2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  photographerName.textContent = name;
+  photographerLocation.textContent = `${city}, ${country}`;
+  photographerTagline.textContent = tagline;
+  photographerImage.setAttribute("src", picture);
 }
+
+function mediaTemplate(media, photographerName) {
+  const mediasContainer = document.querySelector('.photographer-medias');
+  const {title, image, likes, video} = media;
+    const nameArray = photographerName.split(' ');
+    const imageFolder = nameArray[0].toLowerCase();
+    
+    const article = document.createElement( 'article' );
+    let imgVideo;
+    if (video) {
+      const videoPath = `assets/images/${imageFolder}/${video}`;
+      imgVideo = document.createElement('video');
+      imgVideo.setAttribute('controls', '');
+      const videoSource = document.createElement('source');
+      videoSource.setAttribute('src', videoPath);
+      videoSource.setAttribute('type', 'video/mp4');
+      imgVideo.appendChild(videoSource);
+
+    } else {
+      const picture = `assets/images/${imageFolder}/${image}`;
+      imgVideo = document.createElement( 'img' );
+      imgVideo.setAttribute('src', picture);
+    }
+    
+    const itemContent = document.createElement('div');
+    itemContent.className = 'item-content';
+    const h3 = document.createElement( 'h3' );
+    h3.textContent = title;
+    const likesContainer = document.createElement('div');
+    likesContainer.className = 'likes-container';
+    const likesNumber = document.createElement('span');
+    likesNumber.className = 'likes-number';
+    likesNumber.textContent = likes;
+    const likesIcon = document.createElement('i');
+    likesIcon.className = 'fa-solid fa-heart';
+    
+    likesContainer.appendChild(likesNumber);
+    likesContainer.appendChild(likesIcon);
+    itemContent.appendChild(h3);
+    itemContent.appendChild(likesContainer);
+    article.appendChild(imgVideo);
+    article.appendChild(itemContent);
+    
+    mediasContainer.appendChild(article);
+    
+
+
+}
+
+
